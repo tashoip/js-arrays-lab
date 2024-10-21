cfaf472ce5847269eaa763d35b559c6afe854801
Tashoip <parker_tashoi@yahoo.com>
2024-10-20 20:54:49 -0400

    Question 14

--- app.js
+++ app.js
@@ -244,6 +244,23 @@ const numList = numArrays[3];
 console.log('Exercise 13 result:', numList);
 
 
+/*
+Exercise 14: Accessing within nested arrays
+
+1) Retrieve the number `66` from the `numArrays` array. As part of this process
+   do not alter the original `numArrays` array.
+
+2) Assign it to a variable called `num`.
+
+Complete Exercise 14 in the space below:
+*/
+
+
+console.log(numArrays[2][1]);
+const num = numArrays[2][1];
+console.log('Exercise 14 result:', num);
+
+
 
 
 
